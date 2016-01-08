f61837066dc9d2fcfb4f1878cc4a7e08e5c14ea1
Colin Kirkpatrick <colindjk@gmail.com>
2016-01-08 15:34:01 -0500

    updated db config

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app = Flask(__name__)
 # config
 import os
 app.config.from_object(os.environ['APP_SETTINGS'])
-print(os.environ['APP_SETTINGS'])
 
 # create the sqlalchemy object
 db = SQLAlchemy(app)
@@ -68,9 +67,6 @@ def logout():
     session.pop('logged_in', None)
     flash('You were just logged out!')
     return redirect(url_for('welcome'))
-
-# def connect_db():
-    # return sqlite3.connect('posts.db')
 
 if __name__ == '__main__':
     app.run()
