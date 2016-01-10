2775b41ea62ac5d40b895b55d1be694d8871e253
Colin Kirkpatrick <colindjk@gmail.com>
2016-01-09 19:11:41 -0500

    added vimrc for yolo :P

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class BaseConfig(object):
     DEBUG = False
     SECRET_KEY = '\xe4\xf1\x821X\xc8\x03\xec\xf9\xbc\xb6h\xc4\x90\xb8=?\x95>\x04d\xcap\x1c'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    print SQLALCHEMY_DATABASE_URI
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
