82a7cb4252c3e61ea5792c18c6fb7c919a9f94f0
Colin Kirkpatrick <colindjk@gmail.com>
2016-01-12 11:56:57 -0500

    just a test commit

--- db_create.py
+++ db_create.py
@@ -7,6 +7,7 @@ db.create_all()
 #insert etc
 db.session.add(BlogPost("Good", "I\'m good."))
 db.session.add(BlogPost("Well", "I\'m well."))
+db.session.add(BlogPost("postgres", "This post will mean that the postgres db is go!"))
 
 # commit the changes
 db.session.commit()
